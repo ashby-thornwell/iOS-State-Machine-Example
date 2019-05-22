2bec56e5afa4c0c3dfb48a2193a57c6da7ae43f8
“Ashby <ashby.thornwell@mailchimp.com>
2019-05-22 10:05:12 -0400

    Initial Commit

---
+++ StateMachineTestUITests/StateMachineTestUITests.swift
@@ -0,0 +1,34 @@
+//
+//  StateMachineTestUITests.swift
+//  StateMachineTestUITests
+//
+//  Created by Ashby Thornwell on 5/22/19.
+//  Copyright © 2019 Ashby Thornwell. All rights reserved.
+//
+
+import XCTest
+
+class StateMachineTestUITests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+}
